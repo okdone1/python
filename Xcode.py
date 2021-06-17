29d7a903f79edaf73fae4d7f8eab169881d24ea5
Emon tr <86019997+okdone1@users.noreply.github.com>
2021-06-18 01:09:26 +0600

    Create Xcode.py
    https://github.com/okdone1/python.git
    ls
    cd
    ls

---
+++ Xcode.py
@@ -0,0 +1,59 @@
+TatgetTeam
+
+
+user :("Target")
+pass:("Team")
+
+           banner
+pause
+detect_distro
+init_environ
+if [ -f .update ];then
+    echo "All Requirements Found...."
+else
+    echo 'Installing Requirements....'
+    echo .
+    echo .
+    install_deps
+    echo This Script Was Made By SpeedX > .update
+    echo 'Requirements Installed....'
+    pause
+
+
+
+Logo:
+
+
+
+
+               ttttttttttt
+                   tt
+                   tt
+                   00
+                  0000
+                    A
+                   A A
+                  A   A
+                 A     A
+                AaaaaaaaA
+               A         A
+              A           A
+                 RrrrrrR
+                 R      R
+                 R       R
+                 R      R
+                 R     R
+                 R   R
+                 R  R
+                 R R
+                 R  R
+                 R   R
+                 R     R
+                 R      R
+
+
+
+
+inport("033m")y
+sy
+s
